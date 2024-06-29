96fbc77dcfef4b0e1e5812f64018efa9010cac43
Tina Azimi <tazimiaraghi@scentregroup.com>
2024-06-29 12:44:03 +1000

    Add feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import Feature from "./components/Feature";
 
 import * as Sentry from "@sentry/react";
 
@@ -12,6 +13,7 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
+      <Feature />
     </main>
   );
 };
